90f31e5b994bc98e6d58863bb8b180bd451d2e0f
manishgupta577 <67793046+manishgupta577@users.noreply.github.com>
2021-01-20 20:28:29 +0530

    script.js

--- routes/files.js
+++ routes/files.js
@@ -16,12 +16,8 @@ let upload = multer({ storage, limits:{ fileSize: 1000000 * 100 }, }).single('my
 
 router.post('/', (req, res) => {
     upload(req, res, async (err) => {
-      if (!req.file) {
-        return res.json({error:'All fields are required.'});
-      }
-      if(err)
-      {
-          return res.status(500).send({error:err.message})
+      if (err) {
+        return res.status(500).send({ error: err.message });
       }
         const file = new File({
             filename: req.file.filename,
@@ -34,40 +30,42 @@ router.post('/', (req, res) => {
       });
 });
 
-
 router.post('/send', async (req, res) => {
-    
-    const { uuid, emailTo, emailFrom, expiresIn } = req.body;
-    if(!uuid || !emailTo || !emailFrom) {
-        return res.status(422).send({ error: 'All fields are required except expiry.'});
+  const { uuid, emailTo, emailFrom, expiresIn } = req.body;
+  if(!uuid || !emailTo || !emailFrom) {
+      return res.status(422).send({ error: 'All fields are required except expiry.'});
+  }
+  // Get data from db 
+  try {
+    const file = await File.findOne({ uuid: uuid });
+    if(file.sender) {
+      return res.status(422).send({ error: 'Email already sent once.'});
     }
-    // Get data from db 
-    
-      const file = await File.findOne({ uuid: uuid });
-      if(file.sender) {
-        return res.status(422).send({ error: 'Email already sent once.'});
-      }
-      file.sender = emailFrom;
-      file.receiver = emailTo;
-      const response = await file.save();
-
-      //send email
-      const sendMail = require('../services/emailService');
+    file.sender = emailFrom;
+    file.receiver = emailTo;
+    const response = await file.save();
+    // send mail
+    const sendMail = require('../services/mailService');
     sendMail({
       from: emailFrom,
       to: emailTo,
-      subject: 'myShare file sharing',
+      subject: 'inShare file sharing',
       text: `${emailFrom} shared a file with you.`,
       html: require('../services/emailTemplate')({
-                emailFrom:emailFrom, 
+                emailFrom, 
                 downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}?source=email` ,
                 size: parseInt(file.size/1000) + ' KB',
                 expires: '24 hours'
             })
-     
-        });
-        return res.send({sucess:true});
-  
-  });
+    }).then(() => {
+      return res.json({success: true});
+    }).catch(err => {
+      return res.status(500).json({error: 'Error in email sending.'});
+    });
+} catch(err) {
+  return res.status(500).send({ error: 'Something went wrong.'});
+}
 
-module.exports = router;    
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const path = require('path');
-const cors =require('cors');
 
 
 const PORT = process.env.PORT || 3000;
-app.use(express.static('public'));
-app.use(express.json());
-
-
-const connectDB = require('./config/db');
-connectDB();
+const path = require('path');
+const cors =require('cors');
 
 const corsOptions = {
     origin: process.env.ALLOWED_CLIENTS.split(',')
 }
+
 app.use(cors(corsOptions))
+app.use(express.static('public'));
+
+
+
+const connectDB = require('./config/db');
+connectDB();
+app.use(express.json());
+
+
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
@@ -27,8 +32,8 @@ app.use('/files',require('./routes/show'));
 app.use('/files/download',require('./routes/download'));
 
 
-app.listen(PORT,()=>{
 
 
- console.log(`Listening on port ${PORT}.`);
-})
+
+ 
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
